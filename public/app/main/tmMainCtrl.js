c5464a32db9d7dfb795b9475ede20a1230fedf6a
Christopher Baily <chris@oldtowndining.com>
2014-04-22 20:33:28 -0700

    another commit with semi real data on the home page

--- public/app/main/tmMainCtrl.js
+++ public/app/main/tmMainCtrl.js
@@ -1,17 +1,6 @@
-angular.module('app').controller('tmMainCtrl', function($scope){
+angular.module('app').controller('tmMainCtrl', function($scope, tmContract){
 
-    $scope.myVar = "Hello Angular";
 
-    $scope.contracts = [
-        {name:'Johnson', featured:true, published: new Date('10/5/2013')}
 
-        ,{name:'Smith', featured:true, published: new Date('10/5/2013')}
-        ,{name:'Rose', featured:true, published: new Date('10/5/2013')}
-        ,{name:'Garvey', featured:true, published: new Date('10/5/2013')}
-        ,{name:'Russel', featured:true, published: new Date('10/5/2013')}
-        ,{name:'Lopes', featured:true, published: new Date('10/5/2013')}
-        ,{name:'Cey', featured:true, published: new Date('10/5/2013')}
-        ,{name:'Johnson', featured:true, published: new Date('10/5/2013')}
-        ,{name:'Johnson', featured:true, published: new Date('10/5/2013')}
-    ];
+    $scope.contracts = tmContract.query();
 });
