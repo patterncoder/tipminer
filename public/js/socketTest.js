d3294904ded8dacee0ca6ecb249aad1ace216237
patterncoder <chris@oldtowndining.com>
2015-07-25 21:02:54 -0700

    changed hostname for socket.io test

--- public/js/socketTest.js
+++ public/js/socketTest.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect(window.location.host);
   socket.on('news', function (data) {
     console.log(data);
     socket.emit('my other event', { my: 'data' });
